4a740a499bae39c6512549d58176850f26e9dc3b
Adnan Shaikh <adnanshaikh090103@gmail.com>
2024-10-04 15:24:13 +0530

    Worked on featured categories in home page

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+const [itemBg, setItemBg] = useState([
+'#fffceb',
+'#ecffec',
+'#feefea',
+'#fff3eb',
+'#fff3ff',
+'#f2fce4',
+'#feefea',
+'#fffceb',
+'#feefea',
+'#ecffec',
+'#feefea',
+'#fff3eb',
+'#fff3ff',
+'#f2fce4',
+'#feefea',
+'#fffceb',
+'#feefea',
+'#ecffec',
+])
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import HomeBanner from '../../Components/HomeBanner';
 import banner1 from '../../assets/images/banner1.jpg'
+import banner2 from '../../assets/images/banner2.jpg'
+import banner3 from '../../assets/images/banner3.jpg'
+import banner4 from '../../assets/images/banner4.jpg'
 import Button from '@mui/material/Button';
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,6 +13,7 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import ProductItem from '../../Components/ProductItem';
+import HomeCat from '../../Components/HomeCat';
 
 
 const Home = () => {
@@ -23,12 +27,18 @@ const Home = () => {
     };
     return <>
         <HomeBanner />
+        <HomeCat />
         <section className="homeProducts">
             <div className="container">
                 <div className="row">
                     <div className="col-md-3">
-                        <div className="banner">
-                            <img src={banner1} className='cursor w-100  ' alt="" />
+                        <div className="sticky">
+                            <div className="banner">
+                                <img src={banner1} className='cursor w-100  ' alt="" />
+                            </div>
+                            <div className="banner mt-4">
+                                <img src={banner2} className='cursor w-100  ' alt="" />
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-9 productRow">
@@ -79,38 +89,22 @@ const Home = () => {
                             </div>
                             <Button className='viewAllBtn ml-auto'>View all <IoIosArrowRoundForward /></Button>
                         </div>
-                        <div className="product_row w-100 mt-4">
-                            <Swiper
-                                slidesPerView={4}
-                                spaceBetween={0}
-                                pagination={{
-                                    clickable: true,
-                                }}
-                                modules={[Navigation]}
-                                className="mySwiper"
-                            >
-                                <SwiperSlide>
-                                    <ProductItem />
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <ProductItem />
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <ProductItem />
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <ProductItem />
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <ProductItem />
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <ProductItem />
-                                </SwiperSlide>
-
-
-
-                            </Swiper>
+                        <div className="product_row productRow2 w-100 mt-4 d-flex">
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                        </div>
+                        <div className="d-flex mt-4 mb-5 bannerSec">
+                            <div className="banner">
+                                <img src={banner3} className='cursor w-100  ' alt="" />
+                            </div><div className="banner">
+                                <img src={banner4} className='cursor w-100  ' alt="" />
+                            </div>
                         </div>
                     </div>
                 </div>
